fe1ca9726fe2d71124463af5c83dbd3b5c309922
Mazidul <zahid.pb@gmail.com>
2023-08-17 20:22:22 +0600

    Asp.Net\Udemy - .NET 7 Web API & Entity Framework Jumpstart\03 - Entity Framework - .NET 7\011 DELETE Implementations.mp4

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -9,10 +9,7 @@ namespace dotnet_rpg.Services.CharacterService
 {
     public class CharacterService : ICharacterService
     {
-        private static List<Character>  characters = new List<Character>{
-            new Character(),
-            new Character{Id=1 ,Name="Sam"} 
-        };
+
         private readonly IMapper _mapper;
         private readonly DataContext _context;
 
@@ -38,14 +35,16 @@ namespace dotnet_rpg.Services.CharacterService
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
             try
             {
-                var character = characters.FirstOrDefault(c => c.Id == id);
+                var character = _context.Characters.FirstOrDefault(c => c.Id == id);
 
                 if (character == null)  
                     throw new Exception("Charecter with Id '"+id+"' not found.");
 
-                characters.Remove(character);
+                _context.Characters.Remove(character);
+
+                await _context.SaveChangesAsync();
                 
-                serviceResponse.Data = characters.Select(c=>_mapper.Map<GetCharacterDto>(c)).ToList();
+                serviceResponse.Data = _context.Characters.Select(c=>_mapper.Map<GetCharacterDto>(c)).ToList();
             } 
             catch(Exception ex)
             {
