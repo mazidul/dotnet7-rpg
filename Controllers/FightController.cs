991438ddcef997de6694902faedebede6284de99
Mazidul <zahid.pb@gmail.com>
2023-08-30 13:45:28 +0600

    Asp.Net\Udemy - .NET 7 Web API & Entity Framework Jumpstart\06 - Let The Games Begin (More Than Just CRUD) - .NET 7\004 Attack with Skills.mp4

--- Controllers/FightController.cs
+++ Controllers/FightController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
-    [Route("controller")]
+    [Route("[controller]")]
     public class FightController : ControllerBase
     {
         private readonly IFightService _fightService;
@@ -21,7 +21,13 @@ namespace dotnet_rpg.Controllers
         public async Task<ActionResult<ServiceResponse<AttackResultDto>>> WeaponAttack( WeaponAttackDto request)
         {
             return Ok(await _fightService.WeaponAttack(request));
+        } 
+
+        [HttpPost("Skill")]
+        public async Task<ActionResult<ServiceResponse<AttackResultDto>>> SkillAttack( SkillAttackDto request)
+        {
+            return Ok(await _fightService.SkillAttack(request));
         }
-        
+
     }
 }
